535cbd922e5034639a578857107e2f6f6da9319e
Jose Simancas <josesimancas51@gmail.com>
2024-10-15 14:32:23 -0500

    Fix: adjust HOC for authentication

--- components/ProtectedRoute.tsx
+++ components/ProtectedRoute.tsx
@@ -6,11 +6,11 @@ import { useRouter } from 'next/navigation';
 
 export default function ProtectedRoute({ children }: { children: React.ReactNode }) {
     const { isAuthenticated } = useContext(AuthContext);
-    const isAuth = localStorage.getItem('isAuthenticated');
+    const isAuth = localStorage.getItem('isAuthenticated') || null;
     const router = useRouter();
 
     useEffect(() => {
-        if (!isAuthenticated && isAuth === 'false') {
+        if (!isAuthenticated && (isAuth === 'false' || isAuth === null)) {
             router.push('/login');
         }
     }, [isAuthenticated]);
